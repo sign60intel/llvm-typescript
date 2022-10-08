eec562c588415a3c971f290a662a3e0de54ca134
Serguei V. Ignatov <sergign60@gmail.com>
2022-10-08 07:19:33 +0700

    TYPESCRIPT: initialized

---
+++ tslang/include/tslang/Config/config.h.cmake
@@ -0,0 +1,21 @@
+#===-- include/tslang/Config/config.h.cmake ---------------------------------===#
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===------------------------------------------------------------------------===#
+
+/* This generated file is for internal use. Do not include it from headers. */
+
+#ifdef TSLANG_CONFIG_H
+#error config.h can only be included once
+#else
+#define TSLANG_CONFIG_H
+
+
+#define TSLANG_VERSION            "${TSLANG_VERSION}"
+
+#endif
+
+
